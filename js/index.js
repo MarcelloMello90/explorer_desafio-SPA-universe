328ab21330599f35b822d0e1b985e0baf9829d78
Marcelo Melo <marcellomello90@gmail.com>
2023-02-28 21:36:58 +0100

    modified:   style.css, index, js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 import { Router } from './router.js'
 
 const router = new Router()
-router.add('/', "/pages/home.html")
-router.add("/universe", "/pages/universe.html")
-router.add("/explore", "/pages/explore.html")
-router.add(404, "/pages/404.html")
+
+window.route = () => router.Router()
+
+router.add('/', '../pages/home.html')
+router.add('/universe', '../pages/universe.html')
+router.add('/explorer', '../pages/explorer.html')
+router.add(404, '../pages/404.html')
 
 router.handle()
 
 window.onpopstate = () => router.handle()
-window.route = () => router.route()
